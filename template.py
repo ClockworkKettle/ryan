7d1f08b079157c49306a02e4c24f061b088f9bc8
Tim McNamara <timmcnamara@Tims-MacBook-Air.local>
2021-06-25 21:46:13 +0100

    basic options

--- template.py
+++ template.py
@@ -1,6 +1,12 @@
-class loc:
+class level:
     def look_around(self):
         print("you can't see anything")
+    def stand_up(self):
+	print("You're already standing)
+    def look(self, input):
+	print("you look at %s" % input)
+
+	
 
 
 
