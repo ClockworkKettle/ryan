10f83203d07dc4e0b1c8f74260210f4924bf0014
Tim McNamara <timmcnamara@Tims-MacBook-Air.local>
2021-06-25 21:34:44 +0100

    added template import and loc class test

--- game.py
+++ game.py
@@ -1,2 +1,9 @@
+#sys.path.append(".")
+from template import loc
+
 print("You smell really bad")
 print("But you taste really good")
+
+location = loc()
+location.look_around()
+
--- template.py
+++ template.py
@@ -1,5 +1,5 @@
 class loc:
-    def look_around():
+    def look_around(self):
         print("you can't see anything")
 
 
