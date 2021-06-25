cc456ebb93133267e74140bb2ecbc043ad877327
Tim McNamara <timmcnamara@Tims-MacBook-Air.local>
2021-06-25 21:49:57 +0100

    fixed eol

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ class level:
 	def look_around(self):
 		print("you can't see anything")
 	def stand_up(self):
-		print("You're already standing)
+		print("You're already standing")
 	def look(self, input):
 		print("you look at %s" % input)
 
