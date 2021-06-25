9ccc42913d8f0aecb61ccfe8730b6ea8889bbec6
Tim McNamara <timmcnamara@Tims-MacBook-Air.local>
2021-06-25 21:49:13 +0100

    normalized indentation

--- template.py
+++ template.py
@@ -1,10 +1,10 @@
 class level:
-    def look_around(self):
-        print("you can't see anything")
-    def stand_up(self):
-	print("You're already standing)
-    def look(self, input):
-	print("you look at %s" % input)
+	def look_around(self):
+		print("you can't see anything")
+	def stand_up(self):
+		print("You're already standing)
+	def look(self, input):
+		print("you look at %s" % input)
 
 	
 
